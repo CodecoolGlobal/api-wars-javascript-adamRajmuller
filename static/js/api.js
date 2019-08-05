afff26a5777c92c0c2f220b09b7e9dd2314bab8e
adamRajmuller <rajfta@gmail.com>
2019-08-05 18:12:02 +0200

    final commit

--- static/js/api.js
+++ static/js/api.js
@@ -1,7 +1,40 @@
-fetch('https://api.github.com/repos/atom/atom')  // set the path; the method is GET by default, but can be modified with a second parameter
-.then(function (response) {
-    return response.json();
-})  // parse JSON format into JS object
-.then(function (data) {
-    console.log(data.stargazers_count);
-})
+import {drawPlanetTable} from './dom.js'
+
+
+function fetchPlanet(url) {
+    fetch(url)  // set the path; the method is GET by default, but can be modified with a second parameter
+    .then(function (response) {
+        return response.json();
+    })  // parse JSON format into JS object
+    .then(function (data) {
+        drawPlanetTable(data.results);
+
+        let listenerNext = function(){
+            document.querySelector('#btn-next').removeEventListener('click', listenerNext);
+            document.querySelector('#btn-previous').removeEventListener('click', listenerPrevious);
+            fetchPlanet(data.next);
+        };
+        let listenerPrevious = function(){
+            document.querySelector('#btn-next').removeEventListener('click', listenerNext);
+            document.querySelector('#btn-previous').removeEventListener('click', listenerPrevious);
+            fetchPlanet(data.previous);
+        };
+
+        document.querySelector('#btn-next').addEventListener('click', listenerNext);
+        document.querySelector('#btn-previous').addEventListener('click', listenerPrevious);
+})}
+
+
+function getPlanetUrl(page) {
+    return `https://swapi.co/api/planets/?page=${page}`
+}
+
+
+function main() {
+    let currentPage = 1;
+    let planetUrl = getPlanetUrl(currentPage);
+    fetchPlanet(planetUrl);
+}
+
+
+main();
--- static/js/dom.js
+++ static/js/dom.js
@@ -1,3 +1,34 @@
-// import shout from './api.js';
-//
-// let apiData = getApiData('https://swapi.co/api/planets');
+let planetHeader = `<tr>
+                    <thead class="thead-dark">
+                        <th>Name</th>
+                        <th>Diameter</th>
+                        <th>Climate</th>
+                        <th>Terrain</th>
+                        <th>Surface Water</th>
+                        <th>Population</th>
+                        <th>Residents</th>
+                    </thead>
+                   </tr>`;
+
+
+
+export function drawPlanetTable(planets) {
+    document.querySelector('#loading').innerText = '';
+    let innerHtml = planetHeader;
+    for (let i in planets) {
+        innerHtml += `
+            <tr>
+                <td>${planets[i].name}</td>
+                <td>${planets[i].diameter} km</td>
+                <td>${planets[i].climate}</td>
+                <td>${planets[i].terrain}</td>
+                <td>${(planets[i].surface_water !== 'unknown') ? `${planets[i].surface_water}%` : 'Unknown'}</td>
+                <td>${(planets[i].population === 'unknown') ? 'Unkown' : `${planets[i].population} people`}</td>
+                <td>${(planets[i].residents.length > 0) ?
+                `<button type='button' class="btn btn-warning" class="btn btn-primary" data-toggle="modal" data-target="#residents-modal"   data-residents=${planets[i].residents}>${planets[i].residents.length} resident(s)</button>` : 'No residents'}</td>
+            </tr>
+`;
+    }
+    document.querySelector('#table').innerHTML = innerHtml;
+}
+
